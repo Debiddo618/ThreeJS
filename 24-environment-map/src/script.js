92536eb6dc892bbb797dab2b404543ef38bdb099
Debiddo618 <dzheng@terpmail.umd.edu>
2025-07-02 21:19:25 -0700

    complete code structuring lesson

--- 24-environment-map/src/script.js
+++ 24-environment-map/src/script.js
@@ -213,6 +213,8 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
+
+
 /**
  * Animate
  */
