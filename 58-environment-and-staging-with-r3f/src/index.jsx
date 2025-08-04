794387621a69f9943f4fe74f52ea2faa4521922a
Debiddo618 <dzheng@terpmail.umd.edu>
2025-08-04 04:25:10 -0700

    complete environment and staging with R3F

---
+++ 58-environment-and-staging-with-r3f/src/index.jsx
@@ -0,0 +1,34 @@
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience.jsx";
+import Experience2 from "./Experience2.jsx";
+import * as THREE from "three";
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+// const created = ({ gl }) =>
+// {
+//     gl.setClearColor('#ff0000', 1)
+// }
+
+// const created = ({ scene }) =>
+// {
+//     scene.background = new THREE.Color('#ff0000')
+// }
+
+root.render(
+  <Canvas
+    shadows={false}
+    camera={{
+      fov: 45,
+      near: 0.1,
+      far: 200,
+      position: [-4, 3, 6],
+    }}
+    // onCreated={created}
+  >
+    {/* <Experience /> */}
+    <Experience2 />
+  </Canvas>
+);
