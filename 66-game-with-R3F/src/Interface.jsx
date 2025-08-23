23be6e0d41b49a486278ea5c55ea5b09a95a2ac3
Debiddo618 <dzheng@terpmail.umd.edu>
2025-08-22 17:39:46 -0700

    create a marble race game using R3F

---
+++ 66-game-with-R3F/src/Interface.jsx
@@ -0,0 +1,64 @@
+import { useKeyboardControls } from "@react-three/drei";
+import useGame from "./stores/useGame.jsx";
+import { useRef, useEffect } from "react";
+import { addEffect } from "@react-three/fiber";
+
+export default function Interface() {
+  const restart = useGame((state) => state.restart);
+  const phase = useGame((state) => state.phase);
+
+  const forward = useKeyboardControls((state) => state.forward);
+  const backward = useKeyboardControls((state) => state.backward);
+  const leftward = useKeyboardControls((state) => state.leftward);
+  const rightward = useKeyboardControls((state) => state.rightward);
+  const jump = useKeyboardControls((state) => state.jump);
+
+  const time = useRef();
+
+  useEffect(() => {
+    const unsubscribeEffect = addEffect(() => {
+      const state = useGame.getState();
+
+      let elapsedTime = 0;
+
+      if (state.phase === "playing") elapsedTime = Date.now() - state.startTime;
+      else if (state.phase === "ended")
+        elapsedTime = state.endTime - state.startTime;
+
+      elapsedTime /= 1000;
+      elapsedTime = elapsedTime.toFixed(2);
+
+      if (time.current) time.current.textContent = elapsedTime;
+    });
+
+    return () => {
+      unsubscribeEffect();
+    };
+  }, []);
+
+  return (
+    <div className="interface">
+      <div className="time" ref={time}>
+        0.00
+      </div>
+      {phase === "ended" && (
+        <div className="restart" onClick={restart}>
+          Restart
+        </div>
+      )}
+      <div className="controls">
+        <div className="raw">
+          <div className={`key ${forward ? "active" : ""}`}>W</div>
+        </div>
+        <div className="raw">
+          <div className={`key ${leftward ? "active" : ""}`}>a</div>
+          <div className={`key ${backward ? "active" : ""}`}>s</div>
+          <div className={`key ${rightward ? "active" : ""}`}>d</div>
+        </div>
+        <div className="raw">
+          <div className={`key large ${jump ? "active" : ""}`}>Space</div>
+        </div>
+      </div>
+    </div>
+  );
+}
